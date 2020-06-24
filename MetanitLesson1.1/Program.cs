04aa2b445080b9a46de4165ba7d8e9365e55af07
Hayk <hayk.hayr3@gmail.com>
2020-06-25 03:16:29 +0400

    Add project files.

---
+++ MetanitLesson1.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MetanitLesson1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first integer: ");
+            int first = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second integer: ");
+            int second = int.Parse(Console.ReadLine());
+
+            if(first > second)
+            {
+                Console.WriteLine($"{first} > {second}");
+            }
+            else if(first < second)
+            {
+                Console.WriteLine($"{first} < {second}");
+            }
+            else
+            {
+                Console.WriteLine($"{first} = {second}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
